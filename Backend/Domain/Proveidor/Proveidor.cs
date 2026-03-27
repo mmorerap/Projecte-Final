f2e7d3383a021945d91da49d4d8d915255a732c6
mmorerap <marcmoreraprat@gmail.com>
2026-03-27 16:57:21 +0100

    Proveidor nivell Domain

---
+++ Backend/Domain/Proveidor/Proveidor.cs
@@ -0,0 +1,11 @@
+namespace Backend.Domain;
+
+public class Proveedor
+{
+    public string Nombre { get; set; }
+    public string Direccion { get; set; }
+    public string Ciudad { get; set; }
+    public string CodigoPostal { get; set; }
+    public string Pais { get; set; }
+    public string Telefono { get; set; }
+}
