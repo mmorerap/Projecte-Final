91d09facfd6a30ae6a8ed497d871465da3757f7e
mmorerap <marcmoreraprat@gmail.com>
2026-03-27 16:53:21 +0100

    Recepció dades proveidor (DTO request)

---
+++ Backend/Infrastructure/DTO/Proveidor/ProveidorRequest.cs
@@ -0,0 +1,11 @@
+
+public class ProveedorRequest
+{
+    public string Nom { get; set; }
+    public string Direccio { get; set; }
+    public string Ciutat { get; set; }
+    public string CodiPostal { get; set; }
+    public string Pais { get; set; }
+    public string Telefon { get; set; }
+    public string Fax { get; set; }
+}
