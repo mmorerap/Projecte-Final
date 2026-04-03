90c9b0933615e8f527e20b4d9b635d5285bd4e3f
mmorerap <marcmoreraprat@gmail.com>
2026-04-03 17:19:39 +0200

    Creació Application\Porveidor.cs Endpoint

---
+++ Backend/Application/Proveidor.cs
@@ -0,0 +1,19 @@
+using Backend.Infrastructure.DTO;
+using Backend.Services;
+
+namespace Backend.Application.Proveidor;
+
+public static class Proveidor
+{
+    public static void MapProveidorEndpoints(this WebApplication app, DatabaseConnection dbConn)
+    {
+
+
+ 
+        
+    }
+
+
+}
+
+
