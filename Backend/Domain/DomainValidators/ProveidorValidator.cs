e49718e1e4cf0e08f2f14b0aaeb65f48ab34878a
mmorerap <marcmoreraprat@gmail.com>
2026-04-14 16:45:26 +0200

    Creació standar ProveidorValidator.cs

---
+++ Backend/Domain/DomainValidators/ProveidorValidator.cs
@@ -0,0 +1,20 @@
+
+using Backend.Domain;
+using Backend.Common;
+
+namespace dbdemo.Domain.Validators;
+
+public static class ProveidorDValidator
+{
+    public static Result Validate(ProveidorDomain proveidor)
+    {
+        if (proveidor == null)
+            return Result.Failure("La compra no pot ser null", "COMPRA_NULL");
+
+        if (proveidor.Nombre == null)
+            return Result.Failure("El nom de proveidor no pot ser null", "PROVEIDOR_NULL");
+
+
+        return Result.Ok();
+    }
+}
